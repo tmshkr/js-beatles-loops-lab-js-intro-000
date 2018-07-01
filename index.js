9656b980a3ad5ac14b31a4e0ac49ca87f3535c36
Tim Shaker <tmshkr@users.noreply.github.com>
2018-07-01 21:26:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musiciansArray, instrumentsArray){
     var array = []
     for (let i = 0; i < musiciansArray.length; i++){
-        
-    }    
+        array.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`)
+    }
 }
